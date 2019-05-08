14fdabe6ac3b0d400cdd2375ecc09566884f110b
Duc Tai Ly <ductaily@yahoo.de>
2019-05-08 19:46:22 +0200

    #1 the NameGenerator

---
+++ capitalismx-utils/src/main/java/de/uni/mannheim/capitalismx/utils/namegenerator/NameGenerator.java
@@ -0,0 +1,113 @@
+package de.uni.mannheim.capitalismx.utils.namegenerator;
+
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import de.uni.mannheim.capitalismx.utils.data.LocationData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import javax.management.NotificationEmitter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * @author duly
+ */
+public class NameGenerator {
+
+    private static final Logger logger = LoggerFactory.getLogger(NameGenerator.class);
+
+    private NameGenerator() {}
+
+    /**
+     * Makes a get request to https://randomuser.me/api/
+     * @return Returns a json String containing some fakedata
+     * @throws IOException throws Exception if no connection, or URL not resolvable.
+     */
+    private String getGeneratedUser() throws IOException {
+        String userJson = "";
+
+        URL urlForGetRequest = new URL("https://randomuser.me/api/");
+
+        String readLine = null;
+        HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+        conection.setRequestMethod("GET");
+        int responseCode = conection.getResponseCode();
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            while ((readLine = in .readLine()) != null) {
+                response.append(readLine);
+            } in .close();
+            userJson = response.toString();
+        } else {
+            logger.error("GET NOT WORKED");
+        }
+        return userJson;
+    }
+
+    /**
+     * Gets the Fake Person Data from API and parse the Json.
+     * @return Returns a PersonMeta object.
+     */
+    public PersonMeta getGeneratedPersonMeta() {
+        PersonMeta pm = null;
+        try {
+            String jsonData = getGeneratedUser();
+
+            //json string to object conversion
+            JsonParser parser = new JsonParser();
+            JsonElement ele = parser.parse(jsonData);
+            JsonObject jsonObject = ele.getAsJsonObject();
+
+            // logger.info(jsonObject.toString());
+            JsonArray results = jsonObject.getAsJsonArray("results");
+            // logger.info(results.toString());
+
+            JsonElement jname = results.get(0).getAsJsonObject().get("name");
+            JsonElement jgender = results.get(0).getAsJsonObject().get("gender");
+
+            String firstName = toName(jname.getAsJsonObject().get("first").toString());
+
+            String lastName = toName(jname.getAsJsonObject().get("last").toString());
+
+            String title = toName(jname.getAsJsonObject().get("title").toString());
+
+            String eMail = results.get(0).getAsJsonObject().get("email").toString();
+
+            String gender = jgender.toString();
+
+            JsonElement jLoc = results.get(0).getAsJsonObject().get("location");
+
+            String street = jLoc.getAsJsonObject().get("street").toString();
+            String city = jLoc.getAsJsonObject().get("city").toString();
+            int postcode = Integer.parseInt(jLoc.getAsJsonObject().get("postcode").toString());
+
+            LocationData ld = new LocationData(postcode, street, city);
+
+            pm = new PersonMeta().createPersonMeta(firstName, lastName, gender, title, eMail);
+            pm.setLocData(ld);
+        } catch (IOException e) {
+           logger.error(e.getMessage());
+        }
+        return pm;
+    }
+
+
+    public String toName(String name) {
+        //TODO does not work as intended yet
+        String letter = name.substring(0, 1).toUpperCase();
+        return letter + name.substring(1);
+    }
+
+
+}
