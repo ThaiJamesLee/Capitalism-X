1d2cec1a8fbd1cc5952b3e2d3ae100199629f5bb
Jonathan <jsobbe@mail.uni-mannheim.de>
2019-06-20 10:40:22 +0200

    Started implementation of new modules

---
+++ capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/components/hr/EmployeeListViewCell.java
@@ -0,0 +1,8 @@
+package de.uni.mannheim.capitalismx.ui.components.hr;
+
+import de.uni.mannheim.capitalismx.hr.employee.Employee;
+import javafx.scene.control.ListCell;
+
+public class EmployeeListViewCell extends ListCell<Employee> {
+
+}
---
+++ capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/controller/hr/HrEmployeeListController.java
@@ -0,0 +1,28 @@
+package de.uni.mannheim.capitalismx.ui.controller.hr;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import de.uni.mannheim.capitalismx.ui.controller.GameController;
+
+public class HrEmployeeListController extends GameController {
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
