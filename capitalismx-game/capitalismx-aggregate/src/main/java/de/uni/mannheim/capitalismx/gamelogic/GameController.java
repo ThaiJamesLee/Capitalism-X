0103f4ea4389367edf2a0589722902b97a646614
dexzhao <49519922+dexzhao@users.noreply.github.com>
2019-06-26 19:22:36 +0200

    #23 set up initial methods where we aggregate calculations that have to be done at the start at the game and once every game day.

--- capitalismx-game/capitalismx-aggregate/src/main/java/de/uni/mannheim/capitalismx/gamelogic/GameController.java
+++ capitalismx-game/capitalismx-aggregate/src/main/java/de/uni/mannheim/capitalismx/gamelogic/GameController.java
@@ -1,5 +1,9 @@
 package de.uni.mannheim.capitalismx.gamelogic;
 
+import de.uni.mannheim.capitalismx.finance.finance.Finance;
+
+import java.time.LocalDate;
+
 public class GameController {
 
     private static GameController instance;
@@ -15,5 +19,147 @@ public class GameController {
 
     public void nextDay() {
         GameState.getInstance().setGameDate(GameState.getInstance().getGameDate().plusDays(1));
+        this.updateAll();
     }
+
+    private void updateAll() {
+        //TODO update all values of the departments
+        this.updateCompanyEcoIndex();
+        this.updateCustomer();
+        this.updateExternalEvents();
+        this.updateFinance();
+        this.updateHR();
+        this.updateLogistics();
+        this.updateMarketing();
+        this.updateProcurement();
+        this.updateProduction();
+        this.updateWarehouse();
+    }
+
+    private void updateCompanyEcoIndex() {
+
+    }
+
+    private void updateExternalEvents() {
+
+    }
+
+    private void updateCustomer() {
+
+    }
+
+    private void updateFinance() {
+
+    }
+
+    private void updateHR() {
+
+    }
+
+    private void updateLogistics() {
+
+    }
+
+    private void updateMarketing() {
+
+    }
+
+    // TODO once procurement implementation is ready
+    private void updateProcurement() {
+
+    }
+
+    private void updateProduction() {
+
+    }
+
+    private void updateWarehouse() {
+
+    }
+
+    public void start() {
+        GameState.getInstance().initiate();
+        GameThread.getInstance().start();
+    }
+
+    public void pauseGame() {
+        GameThread.getInstance().pause();
+    }
+
+    public void resumeGame() {
+        GameThread.getInstance().unpause();
+    }
+
+    public void setSecondsPerDay(int seconds) {
+        GameThread.getInstance().setSecondsPerDay(seconds);
+    }
+
+    // TODO load game state
+
+    // TODO calculate initial values of respective modules/ departments
+    public void setInitialValues() {
+        this.setInitialCompanyEcoIndexValues();
+        this.setInitialCustomerValues();
+        this.setInitialExternalEventsValues();
+        this.setInitialFinanceValues();
+        this.setInitialHRValues();
+        this.setInitialLogisticsValues();
+        this.setInitialMarketingValues();
+        this.setInitialProcurementValues();
+        this.setInitialProductionValues();
+        this.setInitialWarehouseValues();
+    }
+
+    private void setInitialCompanyEcoIndexValues() {
+
+    }
+
+    private void setInitialExternalEventsValues() {
+
+    }
+
+    private void setInitialCustomerValues() {
+
+    }
+
+    private void setInitialFinanceValues() {
+
+    }
+
+    private void setInitialHRValues() {
+
+    }
+
+    private void setInitialLogisticsValues() {
+
+    }
+
+    private void setInitialMarketingValues() {
+
+    }
+
+    // TODO once procurement implementation is ready
+    private void setInitialProcurementValues() {
+
+    }
+
+    private void setInitialProductionValues() {
+
+    }
+
+    private void setInitialWarehouseValues() {
+
+    }
+
+    public double getCash() {
+        double cash = Finance.getInstance().getCash();
+        return cash;
+    }
+
+    public double getNetWorth(LocalDate gameDate) {
+        double netWorth = Finance.getInstance().calculateNetWorth(gameDate);
+        return netWorth;
+    }
+
+
 }
--- capitalismx-game/capitalismx-aggregate/src/main/java/de/uni/mannheim/capitalismx/gamelogic/GameState.java
+++ capitalismx-game/capitalismx-aggregate/src/main/java/de/uni/mannheim/capitalismx/gamelogic/GameState.java
@@ -1,5 +1,16 @@
 package de.uni.mannheim.capitalismx.gamelogic;
 
+import de.uni.mannheim.capitalismx.customer.CustomerDemand;
+import de.uni.mannheim.capitalismx.customer.CustomerSatisfaction;
+import de.uni.mannheim.capitalismx.ecoindex.CompanyEcoIndex;
+import de.uni.mannheim.capitalismx.external_events.ExternalEvents;
+import de.uni.mannheim.capitalismx.finance.finance.Finance;
+import de.uni.mannheim.capitalismx.hr.department.HRDepartment;
+import de.uni.mannheim.capitalismx.logistic.logistics.Logistics;
+import de.uni.mannheim.capitalismx.marketing.department.MarketingDepartment;
+import de.uni.mannheim.capitalismx.production.Production;
+import de.uni.mannheim.capitalismx.warehouse.Warehousing;
+
 import java.time.LocalDate;
 
 public class GameState {
@@ -17,6 +28,20 @@ public class GameState {
             instance = new GameState();
         }
         return  instance;
+    }
+
+    public void initiate() {
+        HRDepartment.getInstance();
+        Production.getInstance();
+        Warehousing.getInstance();
+        Finance.getInstance();
+        MarketingDepartment.getInstance();
+        Logistics.getInstance();
+        CustomerSatisfaction.getInstance();
+        CustomerDemand.getInstance();
+        // TODO procurement once a procurement department is implemented where it is possible to buy components
+        ExternalEvents.getInstance();
+        CompanyEcoIndex.getInstance();
     }
 
     public LocalDate getGameDate() {
