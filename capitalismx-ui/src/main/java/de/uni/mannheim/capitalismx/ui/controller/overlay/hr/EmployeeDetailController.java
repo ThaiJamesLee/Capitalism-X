97b93d5a402f79bdbe10ebe08c0f8fb0822e18d3
Jonathan <jsobbe@mail.uni-mannheim.de>
2019-08-31 15:37:11 +0200

    #26 Started implementation of example overlay

--- capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/controller/overlay/hr/EmployeeDetailController.java
+++ capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/controller/overlay/hr/EmployeeDetailController.java
@@ -1,22 +1,44 @@
 package de.uni.mannheim.capitalismx.ui.controller.overlay.hr;
 
-import de.uni.mannheim.capitalismx.ui.controller.interfaces.Updateable;
+import java.net.URL;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
 import de.uni.mannheim.capitalismx.ui.controller.overlay.GameOverlayController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class EmployeeDetailController extends GameOverlayController implements Updateable{
+/**
+ * Controller of the overlay, that displays details on a given employee.
+ * 
+ * @author Jonathan
+ *
+ */
+public class EmployeeDetailController extends GameOverlayController {
 
 	@FXML
 	public Label employeeNameLabel;
 
 	@Override
 	public void update() {
-		int employeeId = Integer.parseInt(this.getProperties().getProperty("employeeId", ""));
-		System.out.println("update: " +employeeId);
+		String employeeId = this.getProperties().getProperty("employeeId", "no employee selected");
 		employeeNameLabel.setText("Employee number: " + employeeId);
 	}
-	
-	
-	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Properties getProperties() {
+		return this.properties;
+	}
+
+	@Override
+	public void updateProperties(Properties properties) {
+		this.properties = properties;
+	}
+
 }
