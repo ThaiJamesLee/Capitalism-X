ac6a215baf6f442366f27de0326830689e049931
De Xing Zhao <dzhao@mail.uni-mannheim.de>
2019-09-21 20:13:54 +0200

    #31 tests for customer module

---
+++ capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/controller/module/production/IntroduceProductController.java
@@ -0,0 +1,20 @@
+package de.uni.mannheim.capitalismx.ui.controller.module.production;
+
+import de.uni.mannheim.capitalismx.ui.controller.module.GameModuleController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IntroduceProductController extends GameModuleController {
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
