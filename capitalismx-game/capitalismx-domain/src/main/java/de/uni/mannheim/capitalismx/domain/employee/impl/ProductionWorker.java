858e0a19d59bd21bb5ef0e667bc01fa606e7e2fe
Duc Tai Ly <ductaily@yahoo.de>
2019-10-06 10:24:01 +0200

    #44 leveling mechanism and new employee

---
+++ capitalismx-game/capitalismx-domain/src/main/java/de/uni/mannheim/capitalismx/domain/employee/impl/ProductionWorker.java
@@ -0,0 +1,14 @@
+package de.uni.mannheim.capitalismx.domain.employee.impl;
+
+import de.uni.mannheim.capitalismx.domain.employee.Employee;
+import de.uni.mannheim.capitalismx.utils.data.PersonMeta;
+
+/**
+ * @author duly
+ */
+public class ProductionWorker extends Employee {
+
+    public ProductionWorker(PersonMeta metaData) {
+        super(metaData);
+    }
+}
