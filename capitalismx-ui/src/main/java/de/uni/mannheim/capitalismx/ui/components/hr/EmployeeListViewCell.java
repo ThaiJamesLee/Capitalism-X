0a2e1d9643dc2047f02667e9d2e0ec4c80928384
Jonathan <jsobbe@mail.uni-mannheim.de>
2019-11-12 16:59:04 +0100

    Completely revamped the EmployeeList

--- capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/components/hr/EmployeeListViewCell.java
+++ capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/components/hr/EmployeeListViewCell.java
@@ -4,24 +4,30 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import de.uni.mannheim.capitalismx.domain.employee.Employee;
 import org.controlsfx.control.PopOver;
 
-import de.uni.mannheim.capitalismx.gamelogic.GameState;
+import de.jensd.fx.glyphs.GlyphIconName;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.uni.mannheim.capitalismx.domain.employee.Employee;
 import de.uni.mannheim.capitalismx.domain.employee.Training;
+import de.uni.mannheim.capitalismx.gamelogic.GameState;
 import de.uni.mannheim.capitalismx.ui.application.UIManager;
-import de.uni.mannheim.capitalismx.ui.components.GameViewType;
-import de.uni.mannheim.capitalismx.ui.components.UIElementType;
 import de.uni.mannheim.capitalismx.ui.controller.component.TrainingPopoverController;
 import de.uni.mannheim.capitalismx.ui.controller.general.UpdateableController;
-import de.uni.mannheim.capitalismx.ui.controller.overlay.hr.EmployeeDetailController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.GridPane;
+import javafx.util.Duration;
 
+/**
+ * The custom {@link ListCell} for Employees of the company.
+ * 
+ * @author Jonathan
+ *
+ */
 public class EmployeeListViewCell extends ListCell<Employee> implements UpdateableController {
 
 	@FXML
@@ -30,18 +36,38 @@ public class EmployeeListViewCell extends ListCell<Employee> implements Updateab
 	@FXML
 	private Label wageLabel;
 
-	@FXML
-	private Label skillLabel;
+	private Label satisfactionLabel;
+
+	private FontAwesomeIcon satisfactionIcon = new FontAwesomeIcon();
+
+	private Button trainButton, fireButton;
 
 	@FXML
-	private Button trainButton, fireButton;
+	private Label skillLabel;
 
 	@FXML
 	private GridPane gridPane;
 
 	private FXMLLoader loader;
-	
+
 	private Employee employee;
+
+	private ResourceBundle bundle;
+
+	{
+		// Adds a ChangeListener to the SelectedProperty to toggle the extended cell
+		// when (de-)selected.
+		selectedProperty().addListener((observable, oldValue, newValue) -> {
+			Employee employee = getItem();
+			if (!isEmpty() && employee != null) {
+				if (oldValue && !newValue) {
+					hideExtended();
+				} else if (!oldValue && newValue) {
+					displayExtended();
+				}
+			}
+		});
+	}
 
 	@Override
 	protected void updateItem(Employee employee, boolean empty) {
@@ -52,8 +78,7 @@ public class EmployeeListViewCell extends ListCell<Employee> implements Updateab
 			setGraphic(null);
 		} else {
 			if (loader == null) {
-				//TODO richtiges ressourceBundle übergeben
-				ResourceBundle bundle = UIManager.getResourceBundle();
+				bundle = UIManager.getResourceBundle();
 				loader = new FXMLLoader(
 						getClass().getClassLoader().getResource("fxml/components/employee_list_cell.fxml"), bundle);
 				loader.setController(this);
@@ -63,50 +88,80 @@ public class EmployeeListViewCell extends ListCell<Employee> implements Updateab
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-				// Open Overlay on click
-				// TODO remove header click and maybe set listener only to name label
-				gridPane.setOnMouseClicked(e -> {
-					EmployeeDetailController overlayController = (EmployeeDetailController) UIManager.getInstance()
-							.getGameView(GameViewType.HR).getModule(UIElementType.HR_EMPLOYEES_OVERVIEW).getOverlay()
-							.getController();
-					overlayController.initForDifferentEmployee(employee);
-					UIManager.getInstance().getGamePageController().showOverlay(UIElementType.HR_EMPLOYEES_OVERVIEW);
-				});
 
-				fireButton.setOnAction(e -> {
-					GameState.getInstance().getHrDepartment().fire(employee);
-				});
-
-				FXMLLoader popoverLoader = new FXMLLoader(
-						getClass().getClassLoader().getResource("fxml/components/training_popover.fxml"), bundle);
-				PopOver trainPopover = new PopOver();
-				try {
-					trainPopover.setContentNode(popoverLoader.load());
-					TrainingPopoverController popOverController = ((TrainingPopoverController) popoverLoader
-							.getController());
-					popOverController.init(trainPopover, employee, this);
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-
-				trainButton.setOnAction(e -> {
-					trainPopover.show(trainButton);
-					GameState.getInstance().getHrDepartment().trainEmployee(employee, Training.COURSES);
-				});
+				prepareExtendedCell();
 			}
 
 			update();
-			
+
 			setText(null);
 			setGraphic(gridPane);
 		}
 	}
 
+	/**
+	 * Shows the extended version of the Cell with the buttons for firing and hiring
+	 * the employee.
+	 */
+	private void displayExtended() {
+		if (isSelected()) {
+			gridPane.add(fireButton, 5, 1, 2, 1);
+			gridPane.add(trainButton, 3, 1, 1, 1);
+			gridPane.add(satisfactionLabel, 1, 1);
+			gridPane.add(satisfactionIcon, 0, 1);
+		}
+	}
+
+	/**
+	 * Hides the extended version of the Cell with the buttons for firing and hiring
+	 * the employee.
+	 */
+	private void hideExtended() {
+		if (!isSelected()) {
+			gridPane.getChildren().remove(fireButton);
+			gridPane.getChildren().remove(trainButton);
+			gridPane.getChildren().remove(satisfactionIcon);
+			gridPane.getChildren().remove(satisfactionLabel);
+		}
+	}
+
+	private void prepareExtendedCell() {
+		trainButton = new Button(bundle.getString("employeeList.train"));
+		fireButton = new Button(bundle.getString("employeeList.fire"));
+		trainButton.getStyleClass().add("button_module");
+		fireButton.getStyleClass().add("button_module");
+		satisfactionLabel = new Label(employee.getJobSatisfaction() + "");
+		satisfactionIcon.setIconName("SMILE_ALT");
+		satisfactionIcon.setSize("1.5em");
+
+		fireButton.setOnAction(eFire -> {
+			GameState.getInstance().getHrDepartment().fire(employee);
+		});
+
+		// Prepare the Popover for the trainButton
+		FXMLLoader popoverLoader = new FXMLLoader(
+				getClass().getClassLoader().getResource("fxml/components/training_popover.fxml"), bundle);
+		PopOver trainPopover = new PopOver();
+		try {
+			trainPopover.setContentNode(popoverLoader.load());
+			trainPopover.setFadeInDuration(Duration.ZERO);
+			TrainingPopoverController popOverController = ((TrainingPopoverController) popoverLoader.getController());
+			popOverController.init(trainPopover, employee, this);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		trainButton.setOnAction(eTrain -> {
+			trainPopover.show(trainButton);
+			GameState.getInstance().getHrDepartment().trainEmployee(employee, Training.COURSES);
+		});
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -114,6 +169,7 @@ public class EmployeeListViewCell extends ListCell<Employee> implements Updateab
 		nameLabel.setText(employee.getName());
 		wageLabel.setText((int) employee.getSalary() + " CC");
 		skillLabel.setText(employee.getSkillLevel() + "");
+		satisfactionLabel.setText(employee.getJobSatisfaction() + "");
 	}
 
 }
--- capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/controller/module/hr/EmployeeListController.java
+++ capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/controller/module/hr/EmployeeListController.java
@@ -1,34 +1,36 @@
 package de.uni.mannheim.capitalismx.ui.controller.module.hr;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import de.uni.mannheim.capitalismx.domain.employee.Employee;
-import de.uni.mannheim.capitalismx.domain.employee.EmployeeGenerator;
+import de.uni.mannheim.capitalismx.domain.employee.EmployeeType;
+import de.uni.mannheim.capitalismx.domain.employee.Team;
 import de.uni.mannheim.capitalismx.gamelogic.GameState;
 import de.uni.mannheim.capitalismx.hr.department.HRDepartment;
-
+import de.uni.mannheim.capitalismx.ui.application.UIManager;
 import de.uni.mannheim.capitalismx.ui.components.hr.EmployeeListViewCell;
 import de.uni.mannheim.capitalismx.ui.controller.module.GameModuleController;
+import de.uni.mannheim.capitalismx.ui.eventlisteners.HREventListener;
+import de.uni.mannheim.capitalismx.ui.utils.AnchorPaneHelper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 
 public class EmployeeListController extends GameModuleController {
 
 	@FXML
 	private TabPane employeeTabPane;
 
-	@FXML
-	private ListView<Employee> engineerList;
-	private ObservableList<Employee> engineers;
-
-	@FXML
-	private ListView<Employee> salesList;
-	private ObservableList<Employee> salesPeople;
+	private Map<EmployeeType, ListView<Employee>> listViews;
 
 	@Override
 	public void update() {
@@ -38,26 +40,68 @@ public class EmployeeListController extends GameModuleController {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		HRDepartment hrDep = GameState.getInstance().getHrDepartment();
-		hrDep.hire(EmployeeGenerator.getInstance().generateEngineer(42));
-		hrDep.hire(EmployeeGenerator.getInstance().generateSalesPeople(43));
-		hrDep.hire(EmployeeGenerator.getInstance().generateEngineer(4));
-		engineers = FXCollections.observableArrayList(hrDep.getEngineerTeam().getTeam());
-		salesPeople = FXCollections.observableArrayList(hrDep.getSalesTeam().getTeam());
+		listViews = new HashMap<EmployeeType, ListView<Employee>>();
 
-		engineerList.setItems(engineers);
-		engineerList.setCellFactory(employeeListView -> new EmployeeListViewCell());
-		salesList.setItems(salesPeople);
-		salesList.setCellFactory(employeeListView -> new EmployeeListViewCell());
+		for (EmployeeType type : EmployeeType.values()) {
+			prepareTeamTab(hrDep.getTeamByEmployeeType(type));
+		}
+
+		HREventListener hrEventListener = new HREventListener();
+		hrDep.registerPropertyChangeListener(hrEventListener);
 	}
 
+	/**
+	 * Initially prepares the {@link Tab} for a given {@link Team}, adds the
+	 * necessary {@link ListView} and does some styling.
+	 * 
+	 * @param team The {@link Team} to prepare the {@link Tab} for.
+	 */
+	private void prepareTeamTab(Team team) {
+		Tab teamTab = new Tab(team.getType().getName(UIManager.getResourceBundle().getLocale()));
+		employeeTabPane.getTabs().add(teamTab);
+
+		ObservableList<Employee> teamList = FXCollections.observableArrayList(team.getTeam());
+		ListView<Employee> teamListView = new ListView<Employee>(teamList);
+		teamListView.setCellFactory(employeeListView -> new EmployeeListViewCell());
+		teamListView.setPlaceholder(new Label(UIManager.getResourceBundle().getString("hr.list.placeholder")));
+		listViews.put(team.getType(), teamListView);
+		AnchorPaneHelper.snapNodeToAnchorPane(teamListView);
+
+		AnchorPane rootNode = new AnchorPane();
+		rootNode.getChildren().add(teamListView);
+		teamTab.setContent(rootNode);
+
+		updateEmployeeListView(team.getType(), team.getTeam());
+	}
+
+	/**
+	 * Updates the Items of the {@link ListView} of the given {@link EmployeeType}.
+	 * 
+	 * @param type      The {@link EmployeeType} to update the ListView for.
+	 * @param employees {@link List} of {@link Employee}s containing the new items.
+	 */
+	private void updateEmployeeListView(EmployeeType type, List<Employee> employees) {
+		ListView<Employee> listview = listViews.get(type);
+		listview.setItems(FXCollections.observableArrayList(employees));
+	}
+
+	/**
+	 * Update the {@link Team} of engineers with the List of teammembers.
+	 * 
+	 * @param engineerTeam {@link List} of {@link Employee}s that are now in the
+	 *                     Team.
+	 */
 	public void updateEngineerList(List<Employee> engineerTeam) {
-		engineers = FXCollections.observableArrayList(engineerTeam);
-		engineerList.setItems(engineers);
+		updateEmployeeListView(EmployeeType.ENGINEER, engineerTeam);
 	}
 
+	/**
+	 * Update the {@link Team} of salesPeople with the List of teammembers.
+	 * 
+	 * @param salesTeam {@link List} of {@link Employee}s that are now in the Team.
+	 */
 	public void updateSalesPeopleList(List<Employee> salesTeam) {
-		salesTeam = FXCollections.observableArrayList(salesTeam);
-		salesList.setItems(engineers);
+		updateEmployeeListView(EmployeeType.SALESPERSON, salesTeam);
 	}
 
 }
