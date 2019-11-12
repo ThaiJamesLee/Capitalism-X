e5887928405347cd9e9d383b6c7331ed7e306345
Jonathan <jsobbe@mail.uni-mannheim.de>
2019-11-12 17:00:08 +0100

    #36 Updated HR-Statistics Module

--- capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/controller/module/hr/HrStatisticsController.java
+++ capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/controller/module/hr/HrStatisticsController.java
@@ -3,13 +3,18 @@ package de.uni.mannheim.capitalismx.ui.controller.module.hr;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import de.uni.mannheim.capitalismx.domain.employee.Employee;
 import de.uni.mannheim.capitalismx.domain.employee.EmployeeType;
+import de.uni.mannheim.capitalismx.domain.employee.Team;
 import de.uni.mannheim.capitalismx.gamelogic.GameState;
 import de.uni.mannheim.capitalismx.hr.department.HRDepartment;
+import de.uni.mannheim.capitalismx.ui.components.general.TooltipFactory;
 import de.uni.mannheim.capitalismx.ui.controller.module.GameModuleController;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.util.Duration;
 
 /**
  * Controller for the HRStatistics-Module, that displays stats about employees,
@@ -28,11 +33,14 @@ public class HrStatisticsController extends GameModuleController {
 			employeeSalariesProduction;
 
 	@FXML
+	private Label numberEmployeesHr, employeeSatisfactionHr, employeeProductivityHr, employeeSalariesHr;
+
+	@FXML
 	private Label numberEmployeesOverall, employeeSatisfactionOverall, employeeProductivityOverall,
 			employeeSalariesOverall;
 
 	@FXML
-	private Label firingCost, hiringCost;
+	private Label firingCost, hiringCost, hrCapacity;
 
 	@Override
 	public void update() {
@@ -40,7 +48,7 @@ public class HrStatisticsController extends GameModuleController {
 	}
 
 	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
+	public void initialize(URL url, ResourceBundle resourceBundle) {
 		HRDepartment hrDep = GameState.getInstance().getHrDepartment();
 		employeeProductivityOverall.setText(hrDep.getTotalQualityOfWork() + "");
 		employeeSatisfactionOverall.setText(hrDep.getTotalJSS() + "");
@@ -53,16 +61,31 @@ public class HrStatisticsController extends GameModuleController {
 
 		hiringCost.setText((int) hrDep.getHiringCost() + "");
 		firingCost.setText((int) hrDep.getFiringCost() + "");
+		hrCapacity.setText((int) hrDep.getTotalEmployeeCapacity() + "");
+
+		Tooltip hrCapTooltip = new Tooltip(resourceBundle.getString("hr.stats.capacity.tooltip"));
+		hrCapTooltip.setShowDelay(Duration.millis(50));
+		hrCapacity.setTooltip(hrCapTooltip);
+
 	}
 
+	/**
+	 * Update the stats of the given team, that changed.
+	 * 
+	 * @param typeOfTeam          The {@link EmployeeType} of the updated
+	 *                            {@link Team}.
+	 * @param numberOfTeamMembers The number of {@link Employee}s in the
+	 *                            {@link Team}.
+	 */
 	public void updateTeam(EmployeeType typeOfTeam, int numberOfTeamMembers) {
 		Platform.runLater(() -> {
+			updateNumberOfEmployees();
 			switch (typeOfTeam) {
 			case ENGINEER:
 				updateNumberOfEngineers(numberOfTeamMembers);
 				break;
 			case SALESPERSON:
-				updateNumberOfEmployees(numberOfTeamMembers);
+				updateNumberOfSalesPeople(numberOfTeamMembers);
 				break;
 			default:
 				break;
@@ -78,8 +101,8 @@ public class HrStatisticsController extends GameModuleController {
 		numberEmployeesProduction.setText(numberOfEngineers + "");
 	}
 
-	private void updateNumberOfEmployees(int numOfEmployees) {
-		numberEmployeesOverall.setText(numOfEmployees + "");
+	private void updateNumberOfEmployees() {
+		numberEmployeesOverall.setText(GameState.getInstance().getHrDepartment().getTotalNumberOfEmployees() + "");
 	}
 
 }
