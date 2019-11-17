368a4b7060474eda02cda69f35cf1ea8e8fce5fd
Duc Tai Ly <ductaily@yahoo.de>
2019-11-17 17:13:16 +0100

    #45 save progress

--- capitalismx-game/capitalismx-aggregate/src/main/java/de/uni/mannheim/capitalismx/gamelogic/contract/ContractFactory.java
+++ capitalismx-game/capitalismx-aggregate/src/main/java/de/uni/mannheim/capitalismx/gamelogic/contract/ContractFactory.java
@@ -1,11 +1,13 @@
 package de.uni.mannheim.capitalismx.gamelogic.contract;
 
+import de.uni.mannheim.capitalismx.customer.CustomerDemand;
 import de.uni.mannheim.capitalismx.gamelogic.GameState;
 import de.uni.mannheim.capitalismx.production.Machinery;
 import de.uni.mannheim.capitalismx.production.Product;
 import de.uni.mannheim.capitalismx.production.ProductionDepartment;
 import de.uni.mannheim.capitalismx.utils.random.RandomNumberGenerator;
 
+import java.security.SecureRandom;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +19,7 @@ import java.util.List;
  */
 public class ContractFactory {
 
-    private static List<String> contractorList = new ArrayList<>();
+    private static List<String> contractorList;
 
     static {
         contractorList.add("Metro");
@@ -53,46 +55,78 @@ public class ContractFactory {
     }
 
     /**
-     *  Gets the list of machines and compute the total machinery capacity.
+     *
      * @return Returns the total capacity of machines.
      */
     private static int getProductionCapacity() {
         ProductionDepartment productionDepartment = ProductionDepartment.getInstance();
-        List<Machinery> machines = productionDepartment.getMachines();
 
-        int capacity = 0;
-
-        for(Machinery m : machines) {
-            capacity += m.getMachineryCapacity();
-        }
-        return capacity;
+        return  (int) productionDepartment.getMonthlyAvailableMachineCapacity();
     }
 
     /**
-     *
+     * The period is currently between 1 and 12 months.
      * @return Returns the time in months for the contract to be fulfilled.
      */
-    private int getRandomPeriod() {
+    private static int getRandomPeriod() {
         return RandomNumberGenerator.getRandomInt(1, 12);
     }
 
     /**
+     *
+     * @return Returns true or false with approx. equal probability.
+     */
+    private static boolean isPositive() {
+        return new SecureRandom().nextBoolean();
+    }
+
+    /**
+     *
+     * @return A random factor with equal probability to be in interval [0.0 - 1.0] or [1.0 - 2.0]
+     */
+    private static double generateRandomFactor() {
+        double randomFactor = new SecureRandom().nextDouble();
+        if(isPositive()) {
+            randomFactor += 1;
+        } else {
+            randomFactor = 1 - randomFactor;
+        }
+        return randomFactor;
+    }
+
+    private static double getOfferPrice() {
+        CustomerDemand cd = CustomerDemand.getInstance();
+        // TODO determine price by customer demand / satisfaction.
+
+        return 0.0;
+    }
+
+    /**
      * Creates a contract for the specified product.
-     * @param p The contract for the product.
+     * Sets dates based on the {@link GameState} and the contractor randomly.
+     * @param product The contract for the product.
      * @return Returns the contract.
      */
-    public static Contract getContract(Product p) {
+    public static Contract getContract(final Product product) {
         Contract c = null;
-
         // extract the date from the GameState
         GameState state = GameState.getInstance();
-
         // the start date
         LocalDate date = state.getGameDate();
-
         int year = date.getYear();
 
         String contractor = getRandomContractor();
+
+        int timeToFinish = getRandomPeriod();
+
+        int numProd = (int) (getProductionCapacity() * generateRandomFactor());
+
+        double price = product.getSalesPrice();
+
+
+
+
+        // c = new Contract(contractor, date, product, numProd, );
 
 
         /*
