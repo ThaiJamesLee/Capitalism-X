6799f2b8e0639403a87e307aa167b9905659fc7c
Jonathan <jsobbe@mail.uni-mannheim.de>
2019-12-30 23:22:36 +0100

    #67 First implementation of a Statistics-Module for Warehouse

---
+++ capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/components/warehouse/WarehouseSegment.java
@@ -0,0 +1,11 @@
+package de.uni.mannheim.capitalismx.ui.components.warehouse;
+
+import org.controlsfx.control.SegmentedBar.Segment;
+
+public class WarehouseSegment extends Segment {
+
+	public WarehouseSegment(double value, String text) {
+		super(value, text);
+	}
+
+}
