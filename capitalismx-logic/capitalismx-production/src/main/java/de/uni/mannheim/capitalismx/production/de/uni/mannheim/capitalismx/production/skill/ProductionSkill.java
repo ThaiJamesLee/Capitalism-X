b3bad6b7b6c666318d75d2acfd2b84705b6951da
De Xing Zhao <dzhao@mail.uni-mannheim.de>
2020-01-06 18:14:42 +0100

    #77 first definition of production skills and leveling mechanisms

---
+++ capitalismx-logic/capitalismx-production/src/main/java/de/uni/mannheim/capitalismx/production/de/uni/mannheim/capitalismx/production/skill/ProductionSkill.java
@@ -0,0 +1,29 @@
+package de.uni.mannheim.capitalismx.production.de.uni.mannheim.capitalismx.production.skill;
+
+import de.uni.mannheim.capitalismx.domain.department.DepartmentSkill;
+
+public class ProductionSkill implements DepartmentSkill {
+
+    private int level;
+    private int newMachineCapacity;
+
+    private String description;
+
+    public ProductionSkill(int level, int newMachineCapacity) {
+        this.level = level;
+        this.newMachineCapacity = newMachineCapacity;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getNewMachineCapacity() {
+        return newMachineCapacity;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+}
