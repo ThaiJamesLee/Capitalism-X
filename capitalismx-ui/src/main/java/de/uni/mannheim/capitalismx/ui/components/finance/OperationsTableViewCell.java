e194285615c4a69a0d71945b0fa13f2ab3754242
Sven Tobias Dupper <sdupper@mail.uni-mannheim.de>
2020-01-11 16:07:17 +0100

    #40 improved finance ui

--- capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/components/finance/OperationsTableViewCell.java
+++ capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/components/finance/OperationsTableViewCell.java
@@ -1,8 +1,10 @@
 package de.uni.mannheim.capitalismx.ui.components.finance;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 import de.uni.mannheim.capitalismx.domain.employee.Employee;
 import de.uni.mannheim.capitalismx.domain.employee.EmployeeType;
@@ -14,6 +16,7 @@ import de.uni.mannheim.capitalismx.ui.controller.module.hr.RecruitingListControl
 import de.uni.mannheim.capitalismx.ui.utils.GraphicHelper;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.AnchorPane;
@@ -25,7 +28,7 @@ import javafx.scene.layout.VBox;
  * @author sdupper
  *
  */
-public class OperationsTableViewCell extends ListCell<String[]> {
+public class OperationsTableViewCell implements Initializable {
 
     @FXML
     private Label label1;
@@ -45,66 +48,19 @@ public class OperationsTableViewCell extends ListCell<String[]> {
     @FXML
     private GridPane gridPane;
 
-    private FXMLLoader loader;
-
     private VBox vBox;
+
+    private String[] values;
 
     private String rowName;
 
     private Map<String, OperationsTableViewCell> controllers;
 
-    public OperationsTableViewCell(VBox vBox, Map<String, OperationsTableViewCell> controllers){
+    public OperationsTableViewCell(String[] values, VBox vBox, Map<String, OperationsTableViewCell> controllers){
         super();
         this.vBox = vBox;
         this.controllers = controllers;
-    }
-
-    @Override
-    protected void updateItem(String[] values, boolean empty) {
-        super.updateItem(values, empty);
-        if (empty || values == null) {
-            setText(null);
-            setGraphic(null);
-        } else {
-            if (loader == null) {
-                loader = new FXMLLoader(
-                        getClass().getClassLoader().getResource("fxml/components/operations_table_cell.fxml"),
-                        UIManager.getResourceBundle());
-                loader.setController(this);
-
-                try {
-                    loader.load();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-            controllers.put(values[0], this);
-
-            //gridPane.prefWidthProperty().bind(vBox.prefWidthProperty());
-            gridPane.prefWidthProperty().bind(vBox.widthProperty());
-            //gridPane.setPrefHeight(10);
-            label1.prefWidthProperty().bind(gridPane.widthProperty().divide(5));
-            label2.prefWidthProperty().bind(gridPane.widthProperty().divide(5));
-            label3.prefWidthProperty().bind(gridPane.widthProperty().divide(5));
-            label4.prefWidthProperty().bind(gridPane.widthProperty().divide(5));
-            label5.prefWidthProperty().bind(gridPane.widthProperty().divide(5));
-
-            rowName = values[0];
-            label1.setText(values[1]);
-            label2.setText(values[2]);
-            label3.setText(values[3]);
-            label4.setText(values[4]);
-            label5.setText(values[5]);
-
-            gridPane.setOnMouseClicked(e -> {
-
-            });
-
-            setText(null);
-            setGraphic(gridPane);
-        }
+        this.values = values;
     }
 
     public String getRowName() {
@@ -131,4 +87,28 @@ public class OperationsTableViewCell extends ListCell<String[]> {
         this.label5.setText(text);
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        controllers.put(values[0], this);
+
+        //gridPane.prefWidthProperty().bind(vBox.prefWidthProperty());
+        gridPane.prefWidthProperty().bind(vBox.widthProperty());
+        //gridPane.setPrefHeight(10);
+        label1.prefWidthProperty().bind(gridPane.widthProperty().divide(5));
+        label2.prefWidthProperty().bind(gridPane.widthProperty().divide(5));
+        label3.prefWidthProperty().bind(gridPane.widthProperty().divide(5));
+        label4.prefWidthProperty().bind(gridPane.widthProperty().divide(5));
+        label5.prefWidthProperty().bind(gridPane.widthProperty().divide(5));
+
+        rowName = values[0];
+        label1.setText(values[1]);
+        label2.setText(values[2]);
+        label3.setText(values[3]);
+        label4.setText(values[4]);
+        label5.setText(values[5]);
+
+        gridPane.setOnMouseClicked(e -> {
+
+        });
+    }
 }
--- capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/controller/module/finance/OperationsTableController.java
+++ capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/controller/module/finance/OperationsTableController.java
@@ -13,12 +13,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.util.Callback;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
@@ -41,82 +44,31 @@ public class OperationsTableController  extends GameModuleController {
     public void initialize(URL location, ResourceBundle resources) {
         controllers = new HashMap<>();
 
-        ListView<String[]> headerListView = new ListView<>();
-        ObservableList<String[]> headerList = FXCollections.observableArrayList();
-        headerListView.setItems(headerList);
-        headerListView.setCellFactory(operationsListView -> new OperationsTableViewCell(operationsVBox, controllers));
-        headerList.add(new String[]{"header", "", "0", "0", "0", "0"});
+        FXMLLoader headerLoader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/components/operations_table_cell.fxml"));
+        try {
+            headerLoader.setController(new OperationsTableViewCell(new String[]{"header", "", "0", "0", "0", "0"}, operationsVBox, controllers));
+            Parent headerRow = headerLoader.load();
+            OperationsTableViewCell headerController = headerLoader.getController();
+            operationsVBox.getChildren().add(headerRow);
+            controllers.put("header", headerController);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        ListView<String[]> revenueListView = new ListView<>();
-        ObservableList<String[]> revenueList = FXCollections.observableArrayList();
-        revenueListView.setItems(revenueList);
-        revenueListView.setCellFactory(operationsListView -> new OperationsTableViewCell(operationsVBox, controllers));
-        revenueList.add(new String[]{"sales", UIManager.getLocalisedString("operations.table.sales"), "0", "0", "0", "0"});
-
-        /*ListView<String[]> expensesListView = new ListView<>();
-        ObservableList<String[]> expensesList = FXCollections.observableArrayList();
-        expensesListView.setItems(expensesList);
-        expensesListView.setCellFactory(operationsListView -> new OperationsTableViewCell(operationsVBox, controllers));
-        expensesList.add(new String[]{"hrCosts", UIManager.getLocalisedString("operations.table.hrCosts"), "0", "0", "0", "0"});
-        expensesList.add(new String[]{"warehouseCosts", UIManager.getLocalisedString("operations.table.warehouseCosts"), "0", "0", "0", "0"});
-        expensesList.add(new String[]{"logisticsCosts", UIManager.getLocalisedString("operations.table.logisticsCosts"), "0", "0", "0", "0"});
-        expensesList.add(new String[]{"productionCosts", UIManager.getLocalisedString("operations.table.productionCosts"), "0", "0", "0", "0"});
-        expensesList.add(new String[]{"marketingCosts", UIManager.getLocalisedString("operations.table.marketingCosts"), "0", "0", "0", "0"});
-        expensesList.add(new String[]{"supportCosts", UIManager.getLocalisedString("operations.table.supportCosts"), "0", "0", "0", "0"});*/
-
-        ListView<String[]> hrCostsListView = new ListView<>();
-        ObservableList<String[]> hrCostsList = FXCollections.observableArrayList();
-        hrCostsListView.setItems(hrCostsList);
-        hrCostsListView.setCellFactory(operationsListView -> new OperationsTableViewCell(operationsVBox, controllers));
-        hrCostsList.add(new String[]{"hrCosts", UIManager.getLocalisedString("operations.table.hrCosts"), "0", "0", "0", "0"});
-
-        ListView<String[]> warehouseCostsListView = new ListView<>();
-        ObservableList<String[]> warehouseCostsList = FXCollections.observableArrayList();
-        warehouseCostsListView.setItems(warehouseCostsList);
-        warehouseCostsListView.setCellFactory(operationsListView -> new OperationsTableViewCell(operationsVBox, controllers));
-        warehouseCostsList.add(new String[]{"warehouseCosts", UIManager.getLocalisedString("operations.table.warehouseCosts"), "0", "0", "0", "0"});
-
-        ListView<String[]> logisticsCostsListView = new ListView<>();
-        ObservableList<String[]> logisticsCostsList = FXCollections.observableArrayList();
-        logisticsCostsListView.setItems(logisticsCostsList);
-        logisticsCostsListView.setCellFactory(operationsListView -> new OperationsTableViewCell(operationsVBox, controllers));
-        logisticsCostsList.add(new String[]{"logisticsCosts", UIManager.getLocalisedString("operations.table.logisticsCosts"), "0", "0", "0", "0"});
-
-        ListView<String[]> productionCostsListView = new ListView<>();
-        ObservableList<String[]> productionCostsList = FXCollections.observableArrayList();
-        productionCostsListView.setItems(productionCostsList);
-        productionCostsListView.setCellFactory(operationsListView -> new OperationsTableViewCell(operationsVBox, controllers));
-        productionCostsList.add(new String[]{"productionCosts", UIManager.getLocalisedString("operations.table.productionCosts"), "0", "0", "0", "0"});
-
-        ListView<String[]> marketingCostsListView = new ListView<>();
-        ObservableList<String[]> marketingCostsList = FXCollections.observableArrayList();
-        marketingCostsListView.setItems(marketingCostsList);
-        marketingCostsListView.setCellFactory(operationsListView -> new OperationsTableViewCell(operationsVBox, controllers));
-        marketingCostsList.add(new String[]{"marketingCosts", UIManager.getLocalisedString("operations.table.marketingCosts"), "0", "0", "0", "0"});
-
-        ListView<String[]> supportCostsListView = new ListView<>();
-        ObservableList<String[]> supportCostsList = FXCollections.observableArrayList();
-        supportCostsListView.setItems(supportCostsList);
-        supportCostsListView.setCellFactory(operationsListView -> new OperationsTableViewCell(operationsVBox, controllers));
-        supportCostsList.add(new String[]{"supportCosts", UIManager.getLocalisedString("operations.table.supportCosts"), "0", "0", "0", "0"});
-
-        ListView<String[]> ebitListView = new ListView<>();
-        ObservableList<String[]> ebitList = FXCollections.observableArrayList();
-        ebitListView.setItems(ebitList);
-        ebitListView.setCellFactory(operationsListView -> new OperationsTableViewCell(operationsVBox, controllers));
-        ebitList.add(new String[]{"ebit", UIManager.getLocalisedString("operations.table.ebit"), "0", "0", "0", "0"});
-
-        ListView<String[]> taxListView = new ListView<>();
-        ObservableList<String[]> taxList = FXCollections.observableArrayList();
-        taxListView.setItems(taxList);
-        taxListView.setCellFactory(operationsListView -> new OperationsTableViewCell(operationsVBox, controllers));
-        taxList.add(new String[]{"tax", UIManager.getLocalisedString("operations.table.tax"), "0", "0", "0", "0"});
-
-        ListView<String[]> nopatListView = new ListView<>();
-        ObservableList<String[]> nopatList = FXCollections.observableArrayList();
-        nopatListView.setItems(nopatList);
-        nopatListView.setCellFactory(operationsListView -> new OperationsTableViewCell(operationsVBox, controllers));
-        nopatList.add(new String[]{"nopat", UIManager.getLocalisedString("operations.table.nopat"), "0", "0", "0", "0"});
+        String[] rowNames = {"sales", "hrCosts", "warehouseCosts", "logisticsCosts", "productionCosts", "marketingCosts", "supportCosts",
+                "ebit", "tax", "nopat"};
+        for(String rowName : rowNames){
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/components/operations_table_cell.fxml"));
+            try {
+                loader.setController(new OperationsTableViewCell(new String[]{rowName, UIManager.getLocalisedString("operations.table." + rowName), "0", "0", "0", "0"}, operationsVBox, controllers));
+                Parent headerRow = loader.load();
+                OperationsTableViewCell controller = loader.getController();
+                operationsVBox.getChildren().add(headerRow);
+                controllers.put(rowName, controller);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         /*revenueListView.prefHeightProperty().bind(operationsVBox.heightProperty().divide(5));
         expensesListView.prefHeightProperty().bind(operationsVBox.heightProperty().divide(5));
@@ -126,20 +78,6 @@ public class OperationsTableController  extends GameModuleController {
         //revenueListView.setPrefHeight(50);
         //taxListView.setPrefHeight(200);
         //taxListView.setStyle("-fx-background-color: red;");
-
-        operationsVBox.getChildren().add(headerListView);
-        operationsVBox.getChildren().add(revenueListView);
-        //operationsVBox.getChildren().add(expensesListView);
-        operationsVBox.getChildren().add(hrCostsListView);
-        operationsVBox.getChildren().add(warehouseCostsListView);
-        operationsVBox.getChildren().add(logisticsCostsListView);
-        operationsVBox.getChildren().add(productionCostsListView);
-        operationsVBox.getChildren().add(marketingCostsListView);
-        operationsVBox.getChildren().add(supportCostsListView);
-
-        operationsVBox.getChildren().add(ebitListView);
-        operationsVBox.getChildren().add(taxListView);
-        operationsVBox.getChildren().add(nopatListView);
     }
 
 
