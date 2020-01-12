6fe3d34d3d84b5ce7975f147e3bc0de8a4f5048e
Sven Tobias Dupper <sdupper@mail.uni-mannheim.de>
2020-01-12 17:38:03 +0100

    #40 improved finance overview

--- capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/components/GameModuleDefinition.java
+++ capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/components/GameModuleDefinition.java
@@ -40,9 +40,9 @@ public enum GameModuleDefinition {
 
 	// The modules for HR.
 	FINANCE_OPERATIONS_TABLE("operations_table.fxml", GameViewType.FINANCES, UIElementType.FINANCE_OPERATIONS_TABLE, new GridPosition(5,
-			9, 6, 19), true),
-	FINANCE_OVERVIEW("finance_overview.fxml", GameViewType.FINANCES, UIElementType.FINANCE_OVERVIEW, new GridPosition(0, 19, 0, 6), true),
-	FINANCE_BANKING_SYSTEM("finance_banking_system.fxml", GameViewType.FINANCES, UIElementType.FINANCE_BANKING_SYSTEM, new GridPosition(0, 5, 6, 19), true);
+			9, 7, 19), true),
+	FINANCE_OVERVIEW("finance_overview.fxml", GameViewType.FINANCES, UIElementType.FINANCE_OVERVIEW, new GridPosition(0, 19, 0, 7), true),
+	FINANCE_BANKING_SYSTEM("finance_banking_system.fxml", GameViewType.FINANCES, UIElementType.FINANCE_BANKING_SYSTEM, new GridPosition(0, 5, 7, 19), true);
 	//FINANCE_SALES_CHART("finance_statistics_charts.fxml", GameViewType.FINANCES, UIElementType.FINANCE_SALES_CHART, new GridPosition(14,
 	//		5, 6, 19), true);
 
