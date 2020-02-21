779e63c367d5d0d189b06ebadf604b58407b1836
sdrunima <sdupper@mail.uni-mannheim.de>
2020-02-21 01:01:36 +0100

    #6 improved documentation

--- capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/controller/module/finance/OperationsTableController.java
+++ capitalismx-ui/src/main/java/de/uni/mannheim/capitalismx/ui/controller/module/finance/OperationsTableController.java
@@ -16,11 +16,20 @@ import javafx.scene.Parent;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
+/**
+ * This class represents the operations table (quarterly figures) in the finance UI. It shows information like sales, HR
+ * costs, etc. for the 4 most recent quarters.
+ *
+ * @author sdupper
+ */
 public class OperationsTableController  extends GameModuleController {
 
     @FXML
     private VBox operationsVBox;
 
+    /**
+     * A map of the controller of each table row.
+     */
     private Map<String, OperationsTableViewCell> controllers;
 
     public OperationsTableController() {
@@ -31,6 +40,10 @@ public class OperationsTableController  extends GameModuleController {
         // TODO Auto-generated method stub
     }
 
+    /*
+     * Generates and customizes an OperationsTableViewCell for each row (including the header row) and adds it to the
+     * operations vBox (the "table"). Some rows are separated by a horizontal line.
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         controllers = new HashMap<>();
@@ -81,7 +94,13 @@ public class OperationsTableController  extends GameModuleController {
         operationsVBox.getChildren().add(1, regions[4]);
     }
 
-
+    /**
+     * Updates the specified row of the table according to the specified column values. Moreover, the header row is
+     * updated with the new names (the 4 most recent quarters).
+     * @param rowName The name of the row to be updated.
+     * @param cols The new values for each column in the row.
+     * @param colNames The new values for each column in the header row.
+     */
     public void updateTable(String rowName, String[] cols, String[] colNames){
         Platform.runLater(new Runnable() {
             public void run() {
--- capitalismx-utils/src/main/java/de/uni/mannheim/capitalismx/utils/number/DecimalRound.java
+++ capitalismx-utils/src/main/java/de/uni/mannheim/capitalismx/utils/number/DecimalRound.java
@@ -3,9 +3,19 @@ package de.uni.mannheim.capitalismx.utils.number;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+/**
+ * Rounds decimal numbers.
+ */
 public class DecimalRound {
 
     //https://stackoverflow.com/questions/2808535/round-a-double-to-2-decimal-places
+
+    /**
+     * Rounds decimal numbers.
+     * @param value The value to round.
+     * @param places The desired number of decimal places.
+     * @return
+     */
     public static double round(double value, int places) {
         if (places < 0) throw new IllegalArgumentException();
 
