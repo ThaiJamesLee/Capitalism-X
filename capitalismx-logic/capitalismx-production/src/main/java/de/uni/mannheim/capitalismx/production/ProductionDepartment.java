2ef530767220705979e1fd0b1b82c02f401959b3
dexzhao <49519922+dexzhao@users.noreply.github.com>
2020-02-22 16:15:08 +0100

    #4 quick fix for monthly available machine capacity

--- capitalismx-logic/capitalismx-production/src/main/java/de/uni/mannheim/capitalismx/production/ProductionDepartment.java
+++ capitalismx-logic/capitalismx-production/src/main/java/de/uni/mannheim/capitalismx/production/ProductionDepartment.java
@@ -215,11 +215,11 @@ public class ProductionDepartment extends DepartmentImpl {
         }
     }
 
-    /* This method should always be followed up by updateMonthlyAvailableMachineCapacity and calculate performance metric methods*/
     public void resetMonthlyPerformanceMetrics() {
         this.numberUnitsProducedPerMonth = 0;
         this.monthlyAvailableMachineCapacity = 0;
         this.manufactureEfficiency = 0;
+        this.updateMonthlyAvailableMachineCapacity();
     }
 
     public double buyMachinery(Machinery machinery, LocalDate gameDate) throws NoMachinerySlotsAvailableException {
